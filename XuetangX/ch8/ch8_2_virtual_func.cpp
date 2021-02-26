53dce45ce8415429acf769a7b0a4c2f1e1d67540
yangqinfeng <yangqinfeng@fabu.ai>
2021-02-26 17:52:33 +0800

    feat: default vs code format on save

--- XuetangX/ch8/ch8_2_virtual_func.cpp
+++ XuetangX/ch8/ch8_2_virtual_func.cpp
@@ -5,24 +5,27 @@
 #include <iostream>
 using namespace std;
 
-class Base1 {
-public:
-    virtual void display() const;
-};
-
-class Base2 : public Base1 {
-public:
-    virtual void display() const;
-};
-
-class Derived : public Base2 {
-public:
-    virtual void display() const;
-};
-
-void func(Base1& ptr)
+class Base1
 {
-    ptr.display();
+public:
+  virtual void display() const;
+};
+
+class Base2 : public Base1
+{
+public:
+  virtual void display() const;
+};
+
+class Derived : public Base2
+{
+public:
+  virtual void display() const;
+};
+
+void func(Base1 &ptr)
+{
+  ptr.display();
 }
 
 // 因为公有派生类的对象可以初始化为基类的引用，也可以隐含转为基类的指针
@@ -31,26 +34,26 @@ void func(Base1& ptr)
 
 int main()
 {
-    Base1 b1;
-    Base2 b2;
-    Derived d;
-    func(b1);
-    func(b2);
-    func(d);
-    return 0;
+  Base1 b1;
+  Base2 b2;
+  Derived d;
+  func(b1);
+  func(b2);
+  func(d);
+  return 0;
 }
 
 void Base1::display() const
 {
-    cout << "display() of Base1" << endl;
+  cout << "display() of Base1" << endl;
 }
 
 void Base2::display() const
 {
-    cout << "display() of Base2" << endl;
+  cout << "display() of Base2" << endl;
 }
 
 void Derived::display() const
 {
-    cout << "display() of Derived" << endl;
+  cout << "display() of Derived" << endl;
 }
